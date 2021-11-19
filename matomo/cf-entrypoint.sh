3b0cb67e415dedb1c53c9f797736d5f242e0fe10
Scott Guymer <scott.guymer@philips.com>
2021-11-19 13:09:14 +0100

    Moving cron out to its own container

--- matomo/cf-entrypoint.sh
+++ matomo/cf-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "$@"
-envsubst < /var/www/html/config/config.ini.tmpl.php > /var/www/html/config/config.ini.php
+envsubst < /config.ini.tmpl.php > /var/www/html/config/config.ini.php
 
 chown www-data:www-data /var/www/html/config/config.ini.php
 
